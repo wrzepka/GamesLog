4fa29a16bfaf3774b6b59ed97539b4280194dda7
wrzepka <90323901+wrzepka@users.noreply.github.com>
2025-10-06 19:59:12 +0200

    create db connection logic

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, render_template, request, redirect
+from helpers import close_db
 
 app = Flask(__name__)
 
+# After any request, close connection
+app.teardown_appcontext(close_db)
 @app.route('/')
 def index():
     return render_template('index.html')
